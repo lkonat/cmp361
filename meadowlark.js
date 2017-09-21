cb957cb3c00a37c71aea510f9666c8508434a39a
lassana konate <lkonat@pointpark.edu>
2017-09-21 15:47:43 -0400

    updated

--- meadowlark.js
+++ meadowlark.js
@@ -1,24 +1,24 @@
 var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:"main"});
+var fortune = require("./lib/fortune.js");
 app.engine("handlebars",handlebars.engine);
 app.set("view engine","handlebars");
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname +'/public'));
-
-var fortunes =["Conquer your fears or they will coquer you.",
-               "Rivers need springs.",
-               "Do not fear what you don't know.",
-               "Rivers need springs.",
-               "Rivers need springs.",
-              ];
+app.use( function( req, res, next){
+  res.locals.showTests = app.get(' env') !== 'production' && req.query.test === '1'; 
+  next();
+ });
 
 app.get("/", function(req,res){
   res.render("home");
 });
 app.get("/about", function(req,res){
-var randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)];
-  res.render("about",{fortune:randomFortune}); 
+  res.render("about", {
+          fortune:fortune.getFortune(),
+          pageTestScript: '/qa/tests-about.js'
+  }); 
 });
 
 app.get("/datetime", function(req,res){
